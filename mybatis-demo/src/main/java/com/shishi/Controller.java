537a3866ce075b7d6dc3b4dd75d481113e127a2b
86188 <914104723@qq.com>
2021-08-06 13:09:06 +0800

    mybatis-demo 实际需要修改的代码：第一次提交select all

--- mybatis-demo/src/main/java/com/shishi/Controller.java
+++ mybatis-demo/src/main/java/com/shishi/Controller.java
@@ -1,13 +1,21 @@
 package com.shishi;
 
+import com.shishi.Mapper.WalleMapper;
+import com.shishi.beans.Walle;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class Controller {
 
+    @Resource
+    private WalleMapper mapper;
+
     @RequestMapping("/all")
-    public String getAll() {
-        return "hello";
+    public List<Walle> findAll() {
+        return mapper.findAll();
     }
 }
