8c85a76c64ca5a56aeb304a83e44538e8b1fc81d
86188 <914104723@qq.com>
2021-08-06 13:01:13 +0800

    mybatis-demo add bean

---
+++ mybatis-demo/src/main/java/com/shishi/beans/Walle.java
@@ -0,0 +1,22 @@
+package com.shishi.beans;
+
+public class Walle {
+    private String name;
+    private String cid;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+}
